9bd7be9c711ffd73a797ae15097e1db2b2086c54
jun_yokoguchi <jy.ilcy@gmail.com>
2015-12-28 13:57:08 +0000

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = "logged in as #{@user.name}"
+      session[:user_id] = @user.id
+      flash[:info] ="logged in as #{@user.name}"
       redirect_to @user
     else
       flash[:danger] = 'invalid email/password combination'
