94155e4aa045943e13ba372df97beeb5175b1b17
jun_yokoguchi <jy.ilcy@gmail.com>
2016-01-06 13:53:25 +0000

    profile5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
+  before_action :correct_user, only:[:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -39,5 +40,10 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
