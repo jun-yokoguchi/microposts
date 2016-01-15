fd6ba0b536c8bfe8835773ac1cb95a0c79a64985
jun_yokoguchi <jy.ilcy@gmail.com>
2016-01-15 13:19:13 +0000

    re-re

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,9 @@ class User < ActiveRecord::Base
         following_users.include?(other_user)
     end
     
+    def feed_items
+        Micropost.where(user_id: following_user_ids + [self.id])
+    end
  
     
     has_secure_password
