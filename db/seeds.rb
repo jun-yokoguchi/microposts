1ae52eefe7f6e36fd6f23a9008a8917f5f4c1384
jun_yokoguchi <jy.ilcy@gmail.com>
2016-01-18 15:07:48 +0000

    micropost-heroku2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# リレーションシップ
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
